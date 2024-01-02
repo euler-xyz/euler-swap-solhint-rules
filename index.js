49771bf121f36013347d602ac10829dc8ec55900
Rangel Stoilov <rangelstoilov@gmail.com>
2024-01-02 16:07:50 +0200

    feat: remove all not needed

--- index.js
+++ index.js
@@ -1,22 +1,4 @@
 /* eslint-disable @typescript-eslint/no-var-requires */
-const PrefixInternalFunctionsWithUnderscore = require('./rules/prefixInternalFunctionsWithUnderscore.js');
-const PrefixPrivateFunctionsWithUnderscore = require('./rules/prefixPrivateFunctionsWithUnderscore.js');
-const PrefixStorageVariablesWithSUnderscore = require('./rules/prefixStorageVariablesWithSUnderscore.js');
-const PrefixImmutableVariablesWithI = require('./rules/prefixImmutableVariablesWithI');
-const AllCapsConstantStorageVariables = require('./rules/allCapsConstantStorageVariables.js');
-const NoHardhatImports = require('./rules/noHardhatImports.js');
-const InheritedConstructorArgsNotInContractDefinition = require('./rules/inheritedConstructorArgsNotInContractDefinition.js');
-const NoBlockSingleIfReverts = require('./rules/noBlockSingleIfReverts.js');
-const ExplicitReturns = require('./rules/explicitReturns.js');
+const ExplicitReturns = require("./rules/explicitReturns.js");
 
-module.exports = [
-  PrefixInternalFunctionsWithUnderscore,
-  PrefixPrivateFunctionsWithUnderscore,
-  PrefixStorageVariablesWithSUnderscore,
-  PrefixImmutableVariablesWithI,
-  AllCapsConstantStorageVariables,
-  NoHardhatImports,
-  InheritedConstructorArgsNotInContractDefinition,
-  NoBlockSingleIfReverts,
-  ExplicitReturns,
-];
+module.exports = [ExplicitReturns];
