daccb2899854c8c628c2c8a04e8912221ed99699
Rangel Stoilov <rangelstoilov@gmail.com>
2024-01-04 13:28:45 +0200

    feat: test no return statement case

--- test/explicitReturnTypesSafeCheck.test.js
+++ test/explicitReturnTypesSafeCheck.test.js
@@ -2,18 +2,31 @@ const linter = require("solhint");
 const fs = require("fs");
 const assert = require("assert");
 
+const settings = {
+  plugins: ["euler-swap"],
+  rules: {
+    "euler-swap/explicit-return-types-safe-check": "warn",
+  },
+};
 describe("ExplicitReturnTypesSafeCheck", () => {
   it("should test the unchecked blocks in example contract", () => {
     const uncheckedContract = fs.readFileSync(
       "test/contracts/UncheckedReturn.sol",
       "utf8"
     );
-    const result = linter.processStr(uncheckedContract, {
-      plugins: ["euler-swap"],
-      rules: {
-        "euler-swap/explicit-return-types-safe-check": "warn",
-      },
-    });
+    const result = linter.processStr(uncheckedContract, settings);
     assert.equal(result.reports.length, 0);
+  });
+
+  it("should return an error if no return is detected", () => {
+    const noReturnContract = fs.readFileSync(
+      "test/contracts/NoReturn.sol",
+      "utf8"
+    );
+    const result = linter.processStr(noReturnContract, settings);
+    assert.equal(
+      result.reports[0].message,
+      `Return statements must be written and must explicitly return something; consider "return uint256;"?`
+    );
   });
 });
